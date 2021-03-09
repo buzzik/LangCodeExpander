c4ec8e7a35abeddad586aa217d3f4ab082d6aea8
mr4k <buzzik13@gmail.com>
2021-03-09 12:24:43 +0200

    cleaning

--- app.js
+++ app.js
@@ -6,47 +6,46 @@ const prompt = require('prompt-sync')();
 const inputFile = 'input.txt';
 const exporter = new FileWriter();
 
-
 const inputArr = readFiles(inputFile);
 let resultArr = [];
-let resultFile = '';
-let format = prompt(`1 - html, 2 - csv (1):`, '1');
-let ext = (format === '2') ? '.csv' : '.html';
+const format = prompt(`1 - html, 2 - csv (1):`, '1');
+const ext = format === '2' ? '.csv' : '.html';
 
-(async() => {
-    for (const langCode of inputArr) {
-        let resultString = ''
-        let countryCode = langCode.substring(3);
-        let localeCode = LocaleCode.getLanguageNativeName(langCode);
-        if (format === '1') {
-            resultString = htmlString(langCode, countryCode, localeCode);
-        }
-        if (format === '2') {
-            resultString = txtString(langCode, countryCode, localeCode);
-        }
-
-        resultArr.push(resultString);
-        // console.log(htmlString(langCode, countryCode, localeCode));
+(async () => {
+  for (const langCode of inputArr) {
+    let resultString = '';
+    const countryCode = langCode.substring(3);
+    const localeCode = LocaleCode.getLanguageNativeName(langCode);
+    if (format === '1') {
+      resultString = htmlString(langCode, countryCode, localeCode);
     }
-    resultArr.sort();
-    resultFile = await exporter.writeArray(resultArr, `result${ext}`);
+    if (format === '2') {
+      resultString = txtString(langCode, countryCode, localeCode);
+    }
+    resultArr.push(resultString);
+    // console.log(htmlString(langCode, countryCode, localeCode));
+  }
+  resultArr.sort();
+  await exporter.writeArray(resultArr, `result${ext}`);
 })();
 
 function htmlString(langCode, countryCode, localeCode) {
-    return `<li><span class="sorting-helper" style="display:none">${countryCode}</span><img width="20px" src="../images/svg/${countryCode.toLowerCase()}.svg"/> <span class="lang-name">${capitalizeFirstLetter(localeCode)}(${countryCode})</span></li>`;
+  return `<li><span class="sorting-helper" style="display:none">${countryCode}</span><img width="20px" src="../images/svg/${countryCode.toLowerCase()}.svg"/> <span class="lang-name">${capitalizeFirstLetter(
+    localeCode
+  )}(${countryCode})</span></li>`;
 }
 
 function txtString(langCode, countryCode, localeCode) {
-    return `${countryCode.toLowerCase()}.svg,${langCode},${capitalizeFirstLetter(localeCode)}(${countryCode})`;
+  return `${countryCode.toLowerCase()}.svg,${langCode},${capitalizeFirstLetter(localeCode)}(${countryCode})`;
 }
 
 function readFiles(filepath) {
-    var data = fs.readFileSync(filepath, 'utf8');
-    var content = util.format(data);
-    let array = content.split(/\r?\n|\r/g);
-    return array;
+  const data = fs.readFileSync(filepath, 'utf8');
+  const content = util.format(data);
+  const array = content.split(/\r?\n|\r/g);
+  return array;
 }
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
--- src/FileWriter.js
+++ src/FileWriter.js
@@ -2,43 +2,46 @@ const { Console } = require('console');
 const fs = require('fs');
 
 module.exports = class FileWriter {
-    constructor() {}
-    createFile(name) {
-        name = name || 'checked';
-        let now = new Date();
-        let resFileName = `./result/${now.getTime()}_${name}`;
-        fs.writeFile(resFileName, '', (err) => {
-            if (err) throw err;
-        });
-        console.log(`File created`);
-        return resFileName;
-    }
-    async writeArray(array, fileName, fields) {
-        // fields = fields || false;
-        let path = this.createFile(fileName);
-        let file = fs.createWriteStream(path);
-        file.on('error', function(err) { Console.log(err) });
-        let columnCaptions = "";
-        if (!fields === undefined) {
-            for (const item of fields) {
-                columnCaptions = columnCaptions + item + ",";
-            }
-            file.write(`${columnCaptions}\n`);
-        }
+  constructor() {}
+  createFile(name) {
+    name = name || 'checked';
+    const now = new Date();
+    const resFileName = `./result/${now.getTime()}_${name}`;
+    fs.writeFile(resFileName, '', (err) => {
+      if (err) throw err;
+    });
+    console.log('File created');
+    return resFileName;
+  }
 
-        array.forEach(function(el) {
-            let line = '';
-            if (fields) {
-                for (const n of fields) {
-                    line = line + el[n] + ",";
-                }
-            } else {
-                line = el;
-            }
-            file.write(`${line}\n`);
-        });
-        file.end();
-        console.log(`Data writed to ${path}`);
-        return path;
+  async writeArray(array, fileName, fields) {
+    // fields = fields || false;
+    const path = this.createFile(fileName);
+    const file = fs.createWriteStream(path);
+    file.on('error', function (err) {
+      Console.log(err);
+    });
+    let columnCaptions = '';
+    if (!fields === undefined) {
+      for (const item of fields) {
+        columnCaptions = columnCaptions + item + ',';
+      }
+      file.write(`${columnCaptions}\n`);
     }
+
+    array.forEach(function (el) {
+      let line = '';
+      if (fields) {
+        for (const n of fields) {
+          line = line + el[n] + ',';
+        }
+      } else {
+        line = el;
+      }
+      file.write(`${line}\n`);
+    });
+    file.end();
+    console.log(`Data writed to ${path}`);
+    return path;
+  }
 };
